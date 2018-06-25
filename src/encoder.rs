66a2e7ce640fa4231d4b484b988e3f37eda88c77
TQ Hirsch <tq@p3ki.com>
2018-06-25 17:48:18 +0200

    Enocder docs look good for a first pass

--- src/encoder.rs
+++ src/encoder.rs
@@ -1,4 +1,96 @@
 //! An encoder for bencode. Guarantees that the output string is valid bencode
+//!
+//! # Encoding a structure
+//!
+//! The easiest way to encode a structure is to implement [`Encodable`] for it. For most structures,
+//! this should be very simple:
+//!
+//! ```
+//! # use bencode_zero::encoder::{Encodable, SingleItemEncoder};
+//! # use bencode_zero::Error;
+//! struct Message {
+//!    foo: i32,
+//!    bar: String,
+//! }
+//!
+//! impl Encodable for Message {
+//!     // Atoms have depth one. The struct wrapper adds one level to that
+//!     const MAX_DEPTH: usize = 2;
+//!
+//!     fn encode(&self, encoder: SingleItemEncoder) -> Result<(), Error> {
+//!         encoder.emit_dict(|mut e| {
+//!             // Use e to emit the values
+//!             e.emit_pair(b"bar", &self.bar)?;
+//!             e.emit_pair(b"foo", &self.foo)
+//!         })
+//!     }
+//! }
+//! ```
+//!
+//! Then, messages can be serialized using [`Encodable::to_bytes`]:
+//! ```
+//! # use bencode_zero::encoder::{Encodable, SingleItemEncoder};
+//! # use bencode_zero::Error;
+//! # struct Message {
+//! #    foo: i32,
+//! #    bar: String,
+//! # }
+//! #
+//! # impl Encodable for Message {
+//! #     // Atoms have depth one. The struct wrapper adds one level to that
+//! #     const MAX_DEPTH: usize = 2;
+//! #
+//! #     fn encode(&self, encoder: SingleItemEncoder) -> Result<(), Error> {
+//! #         encoder.emit_dict(|mut e| {
+//! #             // Use e to emit the values
+//! #             e.emit_pair(b"bar", &self.bar)?;
+//! #             e.emit_pair(b"foo", &self.foo)
+//! #         })
+//! #     }
+//! # }
+//! # let result: Result<Vec<u8>, Error> =
+//! Message{
+//!     foo: 1,
+//!     bar: "quux".to_owned(),
+//! }.to_bytes()
+//! # ;
+//! ```
+//!
+//! Most primitive types already implement [`Encodable`].
+//!
+//! # Nesting depth limits
+//!
+//! To allow this to be used on limited platforms, all implementations of [`Encodable`] include a
+//! maximum nesting depth. Atoms (integers and byte strings) are considered to have depth 1. An
+//! object (a list or dict) containing only atoms has depth 2, and in general, an object has a depth
+//! equal to the depth of its deepest member plus one. In some cases, an object doesn't have a
+//! statically known depth. For example, ASTs may be arbitrarily nested. Such objects should
+//! have their depth set to 0, and callers should construct the Encoder manually, adding an
+//! appropriate buffer for the depth:
+//!
+//! ```
+//! # use bencode_zero::encoder::{Encodable, Encoder};
+//! # use bencode_zero::Error;
+//! # fn test() -> Result<Vec<u8>, Error> {
+//! # type ObjectType = u32;
+//! # let object: u32 = 0;
+//! let mut encoder = Encoder::new()
+//!     .with_max_depth(ObjectType::MAX_DEPTH + 10);
+//! encoder.emit(object)?;
+//! encoder.get_output()
+//! # }
+//! ```
+//!
+//! # Error handling
+//!
+//! Once an error occurs during encoding, all future calls to the same encoding stream will fail
+//! early with the same error. It is not defined whether any callback or implementation of
+//! [`Encodable::encode`] is called before returning an error; such callbacks should respond to
+//! failure by bailing out as quickly as possible.
+//!
+//! Not all values in [[`Error`]] can be caused by an encoding operation. Specifically, you only need
+//! to worry about [`UnsortedKeys`](bencode_zero::Error::UnsortedKeys) and
+//! [`NestingTooDeep`](::Error::NestingTooDeep)
 
 use std::collections::BTreeMap;
 use std::collections::HashMap;
@@ -90,7 +182,8 @@ impl Encoder {
         self.state.latch_err(ret)?;
 
         if !value_written {
-            return self.state
+            return self
+                .state
                 .latch_err(Err(Error::InvalidState("No value was emitted".to_owned())));
         }
 
@@ -270,7 +363,7 @@ impl<'a> SingleItemEncoder<'a> {
         self.encoder.emit_list(list_cb)
     }
 
-    /// Emit a sorted dictionary. If the input dictionary is unsorted
+    /// Emit a sorted dictionary. If the input dictionary is unsorted, this will return an error.
     pub fn emit_dict<F>(self, content_cb: F) -> Result<(), Error>
     where
         F: FnOnce(SortedDictEncoder) -> Result<(), Error>,
@@ -509,7 +602,8 @@ where
 
     fn encode(&self, encoder: SingleItemEncoder) -> Result<(), Error> {
         encoder.emit_dict(|mut e| {
-            let mut pairs = self.iter()
+            let mut pairs = self
+                .iter()
                 .map(|(k, v)| (k.as_ref(), v))
                 .collect::<Vec<_>>();
             pairs.sort_by_key(|&(k, _)| k);
