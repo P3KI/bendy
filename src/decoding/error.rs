9f7e874814175113c70a8547ed23b9d571fd2abb
Bruno Kirschner <bruno.kirschner@online.de>
2020-11-13 21:51:15 +0100

    [Decoding] Replace failure with snafu in decoding.

--- src/decoding/error.rs
+++ src/decoding/error.rs
@@ -1,52 +1,54 @@
-#[cfg(not(feature = "std"))]
-use alloc::{str::Utf8Error, string::FromUtf8Error};
-#[cfg(not(feature = "std"))]
-use core::num::ParseIntError;
-
 use alloc::{
     format,
-    string::{String, ToString},
+    str::Utf8Error,
+    string::{FromUtf8Error, String, ToString},
 };
-use core::fmt::{self, Display, Formatter};
+use core::{fmt::Display, num::ParseIntError};
 
 #[cfg(feature = "std")]
-use std::{error::Error as StdError, sync::Arc};
+use std::sync::Arc;
 
-use failure::Fail;
+use snafu::Snafu;
 
-use crate::state_tracker::StructureError;
+use crate::state_tracker;
 
-#[derive(Debug, Clone, Fail)]
+#[derive(Debug, Clone, Snafu)]
 pub struct Error {
-    #[fail(context)]
     context: Option<String>,
-    #[fail(cause)]
-    error: ErrorKind,
+    source: ErrorKind,
 }
 
-/// An enumeration of potential errors that appear during bencode deserialization.
-#[derive(Debug, Clone, Fail)]
+// An enumeration of potential errors that apepar during bencode deserialization.
+#[derive(Debug, Clone, Snafu)]
 pub enum ErrorKind {
     /// Error that occurs if the serialized structure contains invalid semantics.
     #[cfg(feature = "std")]
-    #[fail(display = "malformed content discovered: {}", _0)]
-    MalformedContent(Arc<failure::Error>),
+    #[snafu(display("malformed content discovered: {}", source))]
+    MalformedContent { source: Arc<dyn std::error::Error> },
+
     /// Error that occurs if the serialized structure contains invalid semantics.
     #[cfg(not(feature = "std"))]
-    #[fail(display = "malformed content discovered")]
+    #[snafu(display("malformed content discovered"))]
     MalformedContent,
+
     /// Error that occurs if the serialized structure is incomplete.
-    #[fail(display = "missing field: {}", _0)]
-    MissingField(String),
-    /// Error in the bencode structure (e.g. a missing field end separator).
-    #[fail(display = "bencode encoding corrupted ({})", _0)]
-    StructureError(#[fail(cause)] StructureError),
+    #[snafu(display("missing field: {}", field))]
+    MissingField { field: String },
+
+    /// Error in the bencode structure (e.g. a missing field and seperator).
+    #[snafu(display("bencode encoding corrupted ({})", source))]
+    StructureError { source: state_tracker::StructureError },
+
     /// Error that occurs if the serialized structure contains an unexpected field.
-    #[fail(display = "unexpected field: {}", _0)]
-    UnexpectedField(String),
+    #[snafu(display("unexpected field: {}", field))]
+    UnexpectedField { field: String },
+
     /// Error through an unexpected bencode token during deserialization.
-    #[fail(display = "discovered {} but expected {}", _0, _1)]
-    UnexpectedToken(String, String),
+    #[snafu(display("discovered {} but expected {}", expected, discovered))]
+    UnexpectedToken {
+        expected: String,
+        discovered: String,
+    },
 }
 
 pub trait ResultExt {
@@ -67,47 +69,39 @@ impl Error {
     /// Raised when there is a general error while deserializing a type.
     /// The message should not be capitalized and should not end with a period.
     #[cfg(feature = "std")]
-    pub fn malformed_content(cause: impl Into<failure::Error>) -> Error {
-        let error = Arc::new(cause.into());
-        Self::from(ErrorKind::MalformedContent(error))
+    pub fn malformed_content<SourceT>(source: SourceT) -> Self
+    where
+        SourceT: std::error::Error + Send + Sync + 'static
+    {
+        let error = Arc::new(source);
+        ErrorKind::MalformedContent { source: error }.into()
     }
 
     #[cfg(not(feature = "std"))]
-    pub fn malformed_content<T>(_cause: T) -> Error {
+    pub fn malformed_content<T>(_cause: T) -> Self {
         Self::from(ErrorKind::MalformedContent)
     }
 
-    /// Returns a `Error::MissingField` which contains the name of the field.
-    pub fn missing_field(field_name: impl Display) -> Error {
-        Self::from(ErrorKind::MissingField(field_name.to_string()))
+    // Returns a `Error::MissingField` which contains the name of the field.
+    pub fn missing_field(field_name: impl Display) -> Self {
+        Error::from(ErrorKind::MissingField {
+            field: field_name.to_string(),
+        })
     }
 
     /// Returns a `Error::UnexpectedField` which contains the name of the field.
-    pub fn unexpected_field(field_name: impl Display) -> Error {
-        Self::from(ErrorKind::UnexpectedField(field_name.to_string()))
+    pub fn unexpected_field(field_name: impl Display) -> Self {
+        Error::from(ErrorKind::UnexpectedField {
+            field: field_name.to_string(),
+        })
     }
 
     /// Returns a `Error::UnexpectedElement` which contains a custom error message.
-    pub fn unexpected_token(expected: impl Display, discovered: impl Display) -> Error {
-        Self::from(ErrorKind::UnexpectedToken(
-            expected.to_string(),
-            discovered.to_string(),
-        ))
-    }
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        match &self.context {
-            Some(context) => write!(f, "Error: {} in {}", self.error, context),
-            None => write!(f, "Error: {}", self.error),
-        }
-    }
-}
-
-impl From<StructureError> for Error {
-    fn from(error: StructureError) -> Self {
-        Self::from(ErrorKind::StructureError(error))
+    pub fn unexpected_token(expected: impl Display, discovered: impl Display) -> Self {
+        Error::from(ErrorKind::UnexpectedToken {
+            expected: expected.to_string(),
+            discovered: discovered.to_string(),
+        })
     }
 }
 
@@ -115,41 +109,37 @@ impl From<ErrorKind> for Error {
     fn from(kind: ErrorKind) -> Self {
         Self {
             context: None,
-            error: kind,
+            source: kind,
         }
     }
 }
 
-#[cfg(not(feature = "std"))]
+impl From<state_tracker::StructureError> for Error {
+    fn from(error: state_tracker::StructureError) -> Self {
+        Self::from(ErrorKind::StructureError { source: error })
+    }
+}
+
 impl From<FromUtf8Error> for Error {
     fn from(err: FromUtf8Error) -> Self {
         Self::malformed_content(err)
     }
 }
 
-#[cfg(not(feature = "std"))]
 impl From<Utf8Error> for Error {
     fn from(err: Utf8Error) -> Self {
         Self::malformed_content(err)
     }
 }
 
-#[cfg(not(feature = "std"))]
 impl From<ParseIntError> for Error {
     fn from(err: ParseIntError) -> Self {
         Self::malformed_content(err)
     }
 }
 
-#[cfg(feature = "std")]
-impl<T: StdError + Send + Sync + 'static> From<T> for Error {
-    fn from(error: T) -> Self {
-        Self::malformed_content(error)
-    }
-}
-
 impl<T> ResultExt for Result<T, Error> {
-    fn context(self, context: impl Display) -> Result<T, Error> {
+    fn context(self, context: impl Display) -> Self {
         self.map_err(|err| err.context(context))
     }
 }
