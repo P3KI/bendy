20594dfa0d3e864aa33636a77a9fb6c860fa7b19
OliverUv <oliver.uvman@gmail.com>
2025-04-29 23:04:40 +0200

    Remove dead code in state_tracker stack

--- src/state_tracker/stack.rs
+++ src/state_tracker/stack.rs
@@ -2,23 +2,10 @@
 use alloc::vec::Vec;
 
 pub trait Stack<T> {
-    fn peek_mut(&mut self) -> Option<&mut T>;
-
     fn peek(&self) -> Option<&T>;
-
-    fn replace_top(&mut self, new_value: T);
 }
 
 impl<T> Stack<T> for Vec<T> {
-    fn peek_mut(&mut self) -> Option<&mut T> {
-        let len = self.len();
-        if len == 0 {
-            None
-        } else {
-            Some(&mut self[len - 1])
-        }
-    }
-
     fn peek(&self) -> Option<&T> {
         let len = self.len();
         if len == 0 {
@@ -26,11 +13,5 @@ impl<T> Stack<T> for Vec<T> {
         } else {
             Some(&self[len - 1])
         }
-    }
-
-    fn replace_top(&mut self, new_value: T) {
-        self.peek_mut()
-            .map(|top| *top = new_value)
-            .expect("Shouldn't replace_top with nothing on the stack");
     }
 }
